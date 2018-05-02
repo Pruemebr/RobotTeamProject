b43425d1cf4ae92702993b63df2a85ac10a4b555
pruemebr <pruemebr@rose-hulman.edu>
2018-05-02 09:02:50 -0400

    finished person1

--- sandbox_motors_new/person1_motors.py
+++ sandbox_motors_new/person1_motors.py
@@ -3,7 +3,7 @@ Functions for moving the robot FORWARD and BACKWARD.
 Authors: David Fisher, David Mutchler and Bryce Pruemer.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Implment forward_seconds, then the relevant part of the test function.
+# DONE: 2. Implment forward_seconds, then the relevant part of the test function.
 #          Test and correct as needed.
 #   Then repeat for forward_by_time.
 #   Then repeat for forward_by_encoders.
@@ -30,7 +30,10 @@ def test_forward_backward():
     """
     #forward_seconds(5,50,"brake")
     #forward_by_time(17,100,"brake")
-    forward_by_encoders(10,100,"brake")
+    #forward_by_encoders(10, 100, "brake")
+    #backward_seconds(5, -100, "brake")
+    #backward_by_time(10, -100, "brake")
+    backward_by_encoders(5, -100, "brake")
 
 def forward_seconds(seconds, speed, stop_action):
     """
@@ -73,8 +76,13 @@ def forward_by_time(inches, speed, stop_action):
     """
     #time_s = seconds
     #When speed = 100, in one second, robot moves forward nine inches
-    duration = inches / (9*(speed/100)) #Takes distance and divides by relative speed (relatived because taking distance traveled at speed=100 and findind proportional spped
-    forward_seconds(duration, speed, "brake")
+    if speed >= 0:
+        duration = inches / (9*(speed/100)) #Takes distance and divides by relative speed (relatived because taking distance traveled at speed=100 and findind proportional spped
+        forward_seconds(duration, speed, "brake")
+
+    else:
+        duration = -(inches / (9 * (speed / 100))) # Takes distance and divides by relative speed (relatived because taking distance traveled at speed=100 and findind proportional spped
+        forward_seconds(duration, speed, "brake")
 
 
 def forward_by_encoders(inches, speed, stop_action):
@@ -85,21 +93,34 @@ def forward_by_encoders(inches, speed, stop_action):
       1. Compute the number of degrees the wheels should spin to achieve the desired distance.
       2. Move until the computed number of degrees is reached.
     """
-    deg = (inches/(1.3*3.14159))*(2*3.14159)*(180/3.14159) #number of revolution *2pi rad/rev * 180 deg/rad
-    m.run_to_rel_pos(position_sp=deg)
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    deg = (inches/(1.3*3.14159))*(2*3.14159)*(180/3.14159) #number of revolutions * 2pi rad/rev * 180 deg/rad
+    left_motor.run_to_rel_pos(speed_sp=speed*8)
+    right_motor.run_to_rel_pos(speed_sp=speed*8)
+    left_motor.run_to_rel_pos(position_sp=deg)
+    right_motor.run_to_rel_pos(position_sp=deg)
 
 
 def backward_seconds(seconds, speed, stop_action):
     """ Calls forward_seconds with negative speeds to achieve backward motion. """
-
+    if speed < 0:
+        forward_seconds(seconds, speed, stop_action)
 
 def backward_by_time(inches, speed, stop_action):
     """ Calls forward_by_time with negative speeds to achieve backward motion. """
-
+    if speed < 0:
+        forward_by_time(inches, speed, stop_action)
 
 def backward_by_encoders(inches, speed, stop_action):
     """ Calls forward_by_encoders with negative speeds to achieve backward motion. """
-
+    if speed < 0:
+        forward_by_encoders(-inches, speed, stop_action)
 
 test_forward_backward()
