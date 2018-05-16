a87cf3ec9237f9428257a8e03f248cde9bdc3ff5
chenl4 <chenl4@rose-hulman.edu>
2018-05-16 09:53:01 -0400

    mqtt

--- projects/chenl4/final_project_pc.py
+++ projects/chenl4/final_project_pc.py
@@ -6,6 +6,8 @@
     picks it up, it will sings a song, and its LED light will become red. After driving it
     to where the user wants the cap to be placed, the robot will put down its arm and says
     "Mission Completed!"
+
+    Arthor: Lilin Chen
 """
 
 import tkinter
@@ -16,9 +18,20 @@ import mqtt_remote_method_calls as com
 
 def main():
     mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
 
     root = tkinter.Tk()
     root.title("Red Cap Picker")
 
     main_frame = ttk.Frame(root, padding=50)
     main_frame.grid()
+    photo = tkinter.PhotoImage(file='')
+
+    button1 = ttk.Button(main_frame, image=photo)
+    # The next line is necessary when your root.mainloop() call is outside the
+    # current method. Tkinter has a bug that makes it think the image is ready for
+    # garbage collection, which makes the image fully transparent.
+    # See: http://effbot.org/pyfaq/why-do-my-tkinter-images-not-appear.htm
+    button1.image = photo
+    button1.grid()
+    button1['command'] = lambda: print('hello')
