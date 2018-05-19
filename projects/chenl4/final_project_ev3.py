ddecf6a3485e5e8a74c7eb554309d5e960c94ee3
chenl4 <chenl4@rose-hulman.edu>
2018-05-19 17:09:29 -0400

    mqtt&final project

--- projects/chenl4/final_project_ev3.py
+++ projects/chenl4/final_project_ev3.py
@@ -10,14 +10,17 @@ import robot_controller as robo
 
 def main():
     robot = robo.Snatch3r()
-    mqtt_client = com.MqttClient(robot)
-    mqtt_client.connect_to_pc()
-    # mqtt_client.connect_to_pc("35.194.247.175")  # Off campus IP address of a GCP broker
-    robot.loop_forever()
+
     while robot.running:
         if robot.conditions_for_meeting() == True:
             robot.arm_up()
             robot.found_it()
+            break
+
+    mqtt_client = com.MqttClient(robot)
+    mqtt_client.connect_to_pc()
+    # mqtt_client.connect_to_pc("35.194.247.175")  # Off campus IP address of a GCP broker
+    robot.loop_forever()
 
 
 # ----------------------------------------------------------------------
