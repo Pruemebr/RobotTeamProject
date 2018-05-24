4c06ba6d7129ee34da2b02d8cd83719f8c36a27c
zhangz14 <zhangz14@rose-hulman.edu>
2018-05-24 06:23:29 -0400

    zhangz14

--- projects/zhangz14/final_project_ev3.py
+++ projects/zhangz14/final_project_ev3.py
@@ -1,3 +1,11 @@
+# This project is meant to prevent the robot from knocking into obstacles in the front. When the distance from the robot
+# to the obstacle is less than 50 cm, the LEDs will be yellow. When the distance is less than 20 cm, the LEDs will be
+# red and the robot cannot move forward. The user can get the distance by clicking a button on the Tkinter GUI.
+
+# Author: Zikang Zhang
+
+# Since the class in robot_controller.py is messy, I made another class in this file for my own use.
+
 import mqtt_remote_method_calls as com
 import ev3dev.ev3 as ev3
 import time
@@ -9,16 +17,13 @@ class Snatch3r(object):
         self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
         self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
         self.arm_motor = ev3.MediumMotor(ev3.OUTPUT_A)
-        self.touch_sensor = ev3.TouchSensor
         self.Leds = ev3.Leds
         self.infrared_sensor = ev3.InfraredSensor()
-        self.running = True
 
         assert self.left_motor.connected
         assert self.right_motor.connected
         assert self.arm_motor.connected
         assert self.infrared_sensor
-        assert self.touch_sensor
 
     def forward(self, left_motor_speed, right_motor_speed):
         self.left_motor.run_forever(speed_sp=left_motor_speed)
@@ -27,7 +32,7 @@ class Snatch3r(object):
         if proximity < 70:
             self.Leds.set_color(self.Leds.LEFT, self.Leds.YELLOW)
             self.Leds.set_color(self.Leds.RIGHT, self.Leds.YELLOW)
-        if proximity < 40:
+        if proximity < 28:
             self.stop()
             self.Leds.set_color(self.Leds.LEFT, self.Leds.RED)
             self.Leds.set_color(self.Leds.RIGHT, self.Leds.RED)
@@ -50,37 +55,13 @@ class Snatch3r(object):
         self.Leds.set_color(self.Leds.LEFT, self.Leds.BLACK)
         self.Leds.set_color(self.Leds.RIGHT, self.Leds.BLACK)
 
-    def loop_forever(self):
-        # This is a convenience method that I don't really recommend for most programs other than m5.
-        #   This method is only useful if the only input to the robot is coming via mqtt.
-        #   MQTT messages will still call methods, but no other input or output happens.
-        # This method is given here since the concept might be confusing.
-        self.running = True
-        while self.running:
-            time.sleep(0.1)  # Do nothing (except receive MQTT messages) until an MQTT message calls shutdown.
-
     def shutdown(self):
-        # Modify a variable that will allow the loop_forever method to end. Additionally stop motors and set LEDs green.
-        # The most important part of this method is given here, but you should add a bit more to stop motors, etc.
-        self.running = False
         self.right_motor.stop()
         self.left_motor.stop()
 
-    def arm_up(self):
-        deg = 10 * 360
-        self.arm_motor.run_to_rel_pos(speed_sp=800)
-        self.arm_motor.run_to_rel_pos(position_sp=deg)
-        self.arm_motor.wait_while(self.arm_motor.STATE_RUNNING)
-        self.arm_motor.stop()
-
-    def arm_down(self):
-
-        deg = 10 * 360
-
-        self.arm_motor.run_to_rel_pos(speed_sp=800)
-        self.arm_motor.run_to_rel_pos(position_sp=-deg)
-        self.arm_motor.wait_while(self.arm_motor.STATE_RUNNING)
-        self.arm_motor.stop()
+    def distance(self):
+        distance = self.infrared_sensor.proximity / 100 * 70
+        return distance
 
 
 def main():
--- projects/zhangz14/final_project_pc.py
+++ projects/zhangz14/final_project_pc.py
@@ -1,3 +1,9 @@
+# This project is meant to prevent the robot from knocking into obstacles in the front. When the distance from the robot
+# to the obstacle is less than 50 cm, the LEDs will be yellow. When the distance is less than 20 cm, the LEDs will be
+# red and the robot cannot move forward. The user can get the distance by clicking a button on the Tkinter GUI.
+
+# Author: Zikang Zhang
+
 import tkinter
 from tkinter import ttk
 
@@ -37,37 +43,26 @@ def main():
     forward_button = ttk.Button(main_frame, text="Forward")
     forward_button.grid(row=2, column=1)
     forward_button['command'] = lambda: send_forward(mqtt_client, left_speed_entry, right_speed_entry)
-    root.bind('<Up>', lambda event: send_forward(mqtt_client, left_speed_entry, right_speed_entry))
 
     left_button = ttk.Button(main_frame, text="Left")
     left_button.grid(row=3, column=0)
     left_button['command'] = lambda: send_left(mqtt_client, left_speed_entry, right_speed_entry)
-    root.bind('<Left>', lambda event: send_left(mqtt_client, left_speed_entry, right_speed_entry))
 
     stop_button = ttk.Button(main_frame, text="Stop")
     stop_button.grid(row=3, column=1)
     stop_button['command'] = lambda: send_stop(mqtt_client)
-    root.bind('<space>', lambda event: send_stop(mqtt_client))
 
     right_button = ttk.Button(main_frame, text="Right")
     right_button.grid(row=3, column=2)
     right_button['command'] = lambda: send_right(mqtt_client, left_speed_entry, right_speed_entry)
-    root.bind('<Right>', lambda event: send_right(mqtt_client, left_speed_entry, right_speed_entry))
-
-    up_button = ttk.Button(main_frame, text="Up")
-    up_button.grid(row=5, column=0)
-    up_button['command'] = lambda: send_up(mqtt_client)
-    root.bind('<u>', lambda event: send_up(mqtt_client))
-
-    down_button = ttk.Button(main_frame, text="Down")
-    down_button.grid(row=6, column=0)
-    down_button['command'] = lambda: send_down(mqtt_client)
-    root.bind('<j>', lambda event: send_down(mqtt_client))
 
     back_button = ttk.Button(main_frame, text="Back")
     back_button.grid(row=4, column=1)
     back_button['command'] = lambda: send_backward(mqtt_client, left_speed_entry, right_speed_entry)
-    root.bind('<Down>', lambda event: send_backward(mqtt_client, left_speed_entry, right_speed_entry))
+
+    d_button = ttk.Button(main_frame, text="Distance")
+    d_button.grid(row=5, column=0)
+    d_button['command'] = lambda: send_measure_distance(mqtt_client)
 
     e_button = ttk.Button(main_frame, text="Exit")
     e_button.grid(row=5, column=2)
@@ -105,19 +100,14 @@ def send_right(mqtt_client, l_spd_entry, r_spd_entry):
     mqtt_client.send_message('turn_right', [l_spd, r_spd])
 
 
-def send_up(mqtt_client):
-    print("arm_up")
-    mqtt_client.send_message("arm_up")
-
-
-def send_down(mqtt_client):
-    print("arm_down")
-    mqtt_client.send_message("arm_down")
-
-
 def send_stop(mqtt_client):
     print('stop')
     mqtt_client.send_message('stop')
+
+
+def send_measure_distance(mqtt_client):
+    print('measure_distance')
+    print('The distance to the obstacle in the front is:', mqtt_client.send_message('distance'), "cm.")
 
 
 # Exit button callbacks:
